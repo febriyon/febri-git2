e67455bdc6a184ab11830e91cc9bf76de18a32a9
ramadhanfebriyon <febriyon93@gmail.com>
2020-05-06 16:02:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>remote git</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, officiis nesciunt rem est harum excepturi, nemo ipsum doloremque pariatur, aspernatur nostrum sit eum. Reprehenderit ad enim voluptatum tenetur! Sit, voluptate.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
